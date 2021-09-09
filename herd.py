8fc7b9f42e81a5ac0fd9cd31bf446ecc980d4ce5
andrew alton <joeblk@gmail.com>
2021-09-09 12:30:02 -0800

    Addition of three dinosaurs to herd.py; Stegosaurus, Flinstone, and Megladonindominus Rex

--- herd.py
+++ herd.py
@@ -1,5 +1,12 @@
+from dinosaur import Dinosaur
 class Herd:
     def __init__(self):
-        self.create_herd = ""
+        self.dinosaurs = [] # add three Dinosaur objects
+        # Create a Herd object in main.py and debug
     def create_fleet(self):
         pass
+
+dinosaur1 = Dinosaur('Stegosaurus')
+dinosaur2 = Dinosaur('Flinstone')
+dinosaur3 = Dinosaur('Megladonindominus Rex')
+print(dinosaur1.name)
