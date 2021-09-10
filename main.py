d7fcc258824ad50ef09680a81afdf53204ba1757
andrew alton <joeblk@gmail.com>
2021-09-09 19:18:45 -0800

    Updated main.py with robots and dinosaurs

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from battlefield import Battlefield
+# from battlefield import Battlefield
 from dinosaur import Dinosaur
+from robot import Robot
 
-def __init__(self, dinosaur, battlefield):
-    self.dinosaur = Dinosaur
-    self.battlefield = Battlefield
+robot1 = Robot('A.D.E.E.S.')
+robot2 = Robot('Killbot')
+robot3 = Robot('Slapatron')
+print(robot1.name)
 
+dinosaur1 = Dinosaur('Stegosaurus')
+dinosaur2 = Dinosaur('Flinstone')
+dinosaur3 = Dinosaur('Megladonindominus Rex')
+print(dinosaur1.name)
 # battle1 = robot1.attack(dinosaur1)
 # print("Welcome to Robots vs Dinosaurs!")
 
