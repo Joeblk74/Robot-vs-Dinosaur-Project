506d4d3c12002f78f9155e95879cf0da0448b295
andrew alton <joeblk@gmail.com>
2021-09-09 19:44:43 -0800

    Removed unnecessary print syntax from main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from dinosaur import Dinosaur
 from robot import Robot
 
+print("Welcome to Robots vs Dinosaurs!")
+
 robot1 = Robot('A.D.E.E.S.')
 robot2 = Robot('Killbot')
 robot3 = Robot('Slapatron')
-print(robot1.name)
 
 dinosaur1 = Dinosaur('Stegosaurus')
 dinosaur2 = Dinosaur('Flinstone')
 dinosaur3 = Dinosaur('Megladonindominus Rex')
-print(dinosaur1.name)
-# battle1 = robot1.attack(dinosaur1)
-print("Welcome to Robots vs Dinosaurs!")
 
+# battle1 = robot1.attack(dinosaur1)
 # print(dinosaur1.health)
